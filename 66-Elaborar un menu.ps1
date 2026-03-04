26628dfd41adeba6aacfb564dfb3061ed07ba62c
Ricardo <140122993+ricardovsa8@users.noreply.github.com>
2026-03-04 17:44:21 -0500

    Add files via upload

---
+++ 66-Elaborar un menu.ps1
@@ -0,0 +1,50 @@
+﻿#Elaborando un menu
+Function vermenu {
+Clear-Host
+Write-Host "Iniciando menu..."
+Write-Host "1.Busca"
+Write-Host "2.Crea"
+Write-Host "3.Elimina"
+Write-Host "4.Salir"
+}
+
+vermenu
+
+while (($valor = Read-Host -Prompt "Selecciona una opción") -ne "4") {
+
+switch ($valor){
+            1 {
+                Clear-Host
+                Write-Host "-----------------------"
+                Write-Host "Buscar archivo"
+                Write-Host "-----------------------"
+                pause;
+                break
+            }
+            2 {
+                Clear-Host
+                Write-Host "-----------------------"
+                Write-Host "Crear archivo"
+                Write-Host "-----------------------"
+                pause;
+                break
+            }
+            3 {
+                Clear-Host
+                Write-Host "-----------------------"
+                Write-Host "Eliminar archivo"
+                Write-Host "-----------------------"
+                pause;
+                break
+            }
+            4{
+            "Salir";
+            break
+            }
+            default {
+            Write-Host -ForegroundColor red -BackgroundColor white "Opcion invalido";
+            pause
+            }
+}
+vermenu
+}
